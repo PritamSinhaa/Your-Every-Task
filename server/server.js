dab12bfa67233e9a2b1a04fa7782015f8e0334d1
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-08-24 00:03:35 +0530

    Making auth server

--- server/server.js
+++ server/server.js
@@ -7,12 +7,31 @@ require("dotenv").config();
 const app = express();
 const upload = multer();
 
+// Parser the json
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("From backend");
+});
+
+// Need to change later on
+app.post("/api/auth", upload.none(), (req, res) => {
+  const authData = req.body;
+
+  res.send({ success: true });
+
+  console.log(authData);
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to database");
   })
-  .catch((err) => console.log("Can't be connect to database"));
+  .catch((err) =>
+    console.log("Can't be connect to database, please check your database.")
+  );
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
