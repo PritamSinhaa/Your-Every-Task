8c3a8bed91dc05cc04e7be3b91a9f08134d8d1ba
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-08-25 18:56:56 +0530

    Handling the error

--- server/src/controller/auth.js
+++ server/src/controller/auth.js
@@ -8,15 +8,21 @@ exports.signUp = async (req, res) => {
   const hashPassword = await bcrypt.hash(password, 12);
 
   try {
-    const newUser = new User({
-      name,
-      hashPassword,
-    });
-
+    const newUser = new User({ name, password: hashPassword });
     await newUser.save();
 
-    res.status(201).json({ okay: "Sign up successfully" });
-  } catch {
-    console.log("Something went wrong!");
+    res.status(201).json({ message: "Signup successful!" });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ message: "Username already exists!" });
+    }
+
+    if (error.name === "ValidationError") {
+      const errors = Object.values(error.errors).map((err) => err.message);
+      return res.status(400).json({ message: errors.join(", ") });
+    }
+
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong!" });
   }
 };
