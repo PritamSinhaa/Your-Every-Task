47202717e1a584a6a85091f669d1c636754fd9c9
PritamSinhaa <pritamsinha2323548@gmail.com>
2025-08-25 18:37:50 +0530

    update

--- client/js/auth.js
+++ client/js/auth.js
@@ -1,29 +1,64 @@
-const form = document.querySelector(".form");
+const form = document.querySelector(".form1");
+const password = document.querySelector("#password");
 const passwordError = document.querySelector(".password-error");
+const showPassword = document.querySelector("#show-password");
+const signInBtn = document.querySelector(".sign-in-container");
+const signUpBtn = document.querySelector(".sign-up-container");
 
+// Password type case
+const passwordInputType = {
+  hide: "password",
+  show: "text",
+};
+
+// Keep for back-end
+const authFun = () => {
+  console.log(" Auth is Succesfull");
+};
+
+// Handling form submitting
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
+  const formData = new FormData(form);
+
+  console.log(formData.get("email"));
+
   const email = document.querySelector("#email").value;
   const password = document.querySelector("#password").value;
-
-  console.log({ email, password });
-
-  email.endsWith("@gmail.com")
-    ? console.log("Valid email")
-    : console.log("Invalid email");
+  let emailValid = false;
+  let passwordValid = false;
+  +email.endsWith("@gmail.com") ? (emailValid = false) : (emailValid = true);
 
   if (
     password.length > 8 &&
     /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/.test(password)
   ) {
     passwordError.style.display = "none";
+    passwordValid = true;
   } else {
     passwordError.style.display = "block";
+    passwordValid = false;
   }
+
+  console.log(passwordValid, emailValid);
+
+  passwordValid && emailValid
+    ? authFun()
+    : console.log("Authentification fail");
 });
 
-// include symbol at least 8 character with number | password verification
+// Password show and hide
+showPassword.addEventListener("click", () => {
+  password.getAttribute("type") === passwordInputType.hide
+    ? password.setAttribute("type", passwordInputType.show)
+    : password.setAttribute("type", passwordInputType.hide);
+});
+
+//sign in button
+
+signInBtn.addEventListener("click", (e) => {});
+
 // When the form is not valid refocus on the error part
 // Add click listener in eye button to hidden or show the password
 // Make function about sign in and sign up for switching btw
