0d2daa3888667bfeaf03b3beda663a51ecc9b84d
Meshil Maring <dsmeshilmaring13@gmail.com>
2025-09-17 21:12:55 +0530

    Connect ollama with ngrok

---
+++ client/js/ai-prompt.js
@@ -0,0 +1,30 @@
+const $ = (id) => document.getElementById(id);
+
+const form = $("form");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+
+  // Get user input from your form field
+  const formData = new FormData(form);
+  const userPrompt = formData.get("input"); // input is the name="" of your input field
+
+  // Send JSON to Ollama
+  const res = await fetch("https://64b5492b7f60.ngrok-free.app/api/generate", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "gemma3:1b",
+      prompt: userPrompt,
+      stream: false,
+    }),
+  });
+
+  const data = await res.json();
+
+  console.log(data);
+
+  $("response").textContent = data.response;
+});
